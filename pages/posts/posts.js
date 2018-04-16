e44aae7a54cb16b2ea258b316f75cac2cc8521a2
xueqiwei <xueqiwei@163.com>
2018-04-16 18:25:49 +0800

    收藏功能

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -5,10 +5,11 @@ Page({
     post_key: []
   },
   onPostTap:function(event){
+    var postId = event.currentTarget.dataset.postid
     //下面可以获取自定义属性
     // console.log(event.currentTarget.dataset.postid)
     wx.navigateTo({
-      url: 'post-detail/post-detail'
+      url: 'post-detail/post-detail?id='+postId
     })
   },
   onLoad: function (options) {
