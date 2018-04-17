ac1716620dcf5d299c0cf384b04d88fe07a687ac
xueqiwei <xueqiwei@163.com>
2018-04-17 18:25:22 +0800

    简单的格式化了下

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -22,24 +22,24 @@ Page({
     })
     //从缓存中获取存储的对象，键为collect_Info,
     var collectInfo = wx.getStorageSync('collect_Info');
-    //如果存在该对象，
+    //逻辑判断
     if (collectInfo) {
-      if (collectInfo[postId]) {
-        var collectdetail = collectInfo[postId]
-      } else {
+      //处理逻辑放在外面了
+      if(!collectInfo[postId]){
         collectInfo[postId] = false
-        wx.setStorageSync('collect_Info', collectInfo)
       }
     } else {
-      //如果尚未存在，泽创建对象，依据postID,设置完成后存入缓存
+      //如果尚未存在，则创建对象，依据postID,设置完成后存入缓存
       var collectInfo = {}
       collectInfo[postId] = false
-      wx.setStorageSync('collect_Info', collectInfo)
     }
-    //缓存处理完成后设置当前页面的data
+    //缓存处理以及设置当前页面的data
+    wx.setStorageSync('collect_Info', collectInfo)
     this.setData({
       collected: collectInfo[postId]
     })
+
+    //播放设置
     var that = this;
     wx.onBackgroundAudioPlay(function(){
       that.setData({
@@ -51,13 +51,11 @@ Page({
         isplaying: false
       })
     })
-    
     if (app.globalData.g_isplaying && app.globalData.g_currentpostId==postId){
       this.setData({
         isplaying:true
       })
     }
-
     this.setAudio();
   },
   setAudio: function (event) {
